f4418ebfe1e53f791536c100762ac37a9040d3ab
Matteriel-Git <matthew.alpert@gmail.com>
2025-05-19 13:49:24 -0700

    updates

--- src/components/ForecastHourly.tsx
+++ src/components/ForecastHourly.tsx
@@ -1,2 +1,16 @@
 /* Hourly forecast - Scrollable weather slots */ 
 import { mockWeatherData } from "lib/mockData/weather";
+
+const ForecastHourly = () => {
+    return (
+        <div className="grid grid-cols-3 gap-2">
+        {mockWeatherData.hourly.map((hour, i) => (
+            <div key={i} className="rounded bg-white/10 p-2 text-center">
+                <p>{hour.time}</p>
+                <img src="{`/icons/${hour.icon}.png`}" alt="{hour.icon}" />
+                <p>{hour.temp_f}°F</p>
+            </div>
+        ))}
+        </div>
+    );
+};
