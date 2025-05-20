4fd6295685097adb7663aa5875885e1572d41844
Matteriel-Git <matthew.alpert@gmail.com>
2025-05-19 22:39:59 -0700

    updates

--- src/components/MetrixBox.tsx
+++ src/components/MetrixBox.tsx
@@ -1,1 +1,16 @@
 /* Wind, humidity, etc. - grid of small info cartds */
+
+import { mockWeatherData } from "lib/mockData/weather";
+
+const MetrixBox = () => {
+    return (
+        <div className="">
+            <p>{}</p>
+            <p>{}</p>
+            <p>{}</p>
+            <p>{}</p>
+        </div>
+    );
+};
+
+export default MetrixBox;
