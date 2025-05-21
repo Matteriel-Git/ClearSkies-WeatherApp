7dff373ada8d76339e9527aa92097ba26db1b7fa
Matteriel-Git <matthew.alpert@gmail.com>
2025-05-20 17:32:16 -0700

    updates

--- src/components/MetricBox.tsx
+++ src/components/MetricBox.tsx
@@ -3,12 +3,13 @@
 import { mockWeatherData } from "lib/mockData/weather";
 
 const MetricBox = () => {
+    const current = mockWeatherData.current;
     return (
         <div className="">
-            <p>{}</p>
-            <p>{}</p>
-            <p>{}</p>
-            <p>{}</p>
+            <p>{current.wind_mph} {current.wind_dir}</p>
+            <p>{current.humidity} {current.dewpoint_f}</p>
+            <p>{current.pressure_mb}</p>
+            <p>{current.uv}</p>
         </div>
     );
 };
