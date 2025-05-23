fbab1374960b4c4ca8c6071d6c69b1f127b9f6a3
Matteriel-Git <matthew.alpert@gmail.com>
2025-05-22 22:06:17 -0700

    updates

--- src/components/AirQualityBar.tsx
+++ src/components/AirQualityBar.tsx
@@ -4,7 +4,7 @@
 import { mockWeatherData } from "lib/mockData/weather";
 
 const AirQualityBar = () => {
-    
+
     return (
         <div className="text-center">
             <h1>Air Quality Bar</h1>
@@ -12,4 +12,5 @@ const AirQualityBar = () => {
         </div>
     )
 }
+
 export default AirQualityBar;
